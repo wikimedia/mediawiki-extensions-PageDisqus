4b99f48b3babe954e50201976cf49f78d564b7b7
Siebrand Mazeland <siebrand@kitano.nl>
2014-04-01 14:42:03 +0200

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: Ibc825e7c43f39a0a0ffb70665b822bdff959123d

--- PageDisqus.php
+++ PageDisqus.php
@@ -9,18 +9,18 @@
  * Revised by Angel Guzmán Maeso <shakaran at gmail dot com>
  * Updated by Luis Felipe Schenone <schenonef@gmail.com>
  * @license GPL
- * @version 0.2
  */
 
 $wgExtensionCredits['other'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'PageDisqus',
 	'descriptionmsg' => 'pagedisqus-desc',
-	'version'        => 0.2,
+	'version'        => '0.3.0',
 	'author'         => array( 'Michael Platzer', 'Luis Felipe Schenone' ),
 	'url'            => 'http://www.mediawiki.org/wiki/Extension:PageDisqus'
 );
 
+$wgMessagesDirs['PageDisqus'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['PageDisqus'] = __DIR__ . '/PageDisqus.i18n.php';
 $wgAutoloadClasses['PageDisqus'] = __DIR__ . '/PageDisqus.body.php';
 
