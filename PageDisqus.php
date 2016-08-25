042d9dbaedc422fda0b9ec47a52d8d7d7950de0f
Sophivorus <schenonef@gmail.com>
2016-08-24 21:03:36 -0300

    Convert extension to registration
    Also add .gitignore
    
    Change-Id: I7703c1b5aa4ef4d49a91202f54d259b99aae957c

--- PageDisqus.php
+++ PageDisqus.php
@@ -1,28 +1,93 @@
 <?php
-/**
- * Disqus integration extension
- *
- * @file
- * @ingroup Extensions
- *
- * @author Michael Platzer <michael.platzer@gmail.com>
- * Revised by Angel Guzmán Maeso <shakaran at gmail dot com>
- * Updated by Luis Felipe Schenone <schenonef@gmail.com>
- * @license GPL
- */
 
-$wgExtensionCredits['other'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'PageDisqus',
-	'descriptionmsg' => 'pagedisqus-desc',
-	'version'        => '0.3.0',
-	'author'         => array( 'Michael Platzer', 'Luis Felipe Schenone' ),
-	'url'            => 'http://www.mediawiki.org/wiki/Extension:PageDisqus'
-);
+class PageDisqus {
 
-$wgMessagesDirs['PageDisqus'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['PageDisqus'] = __DIR__ . '/PageDisqus.i18n.php';
-$wgAutoloadClasses['PageDisqus'] = __DIR__ . '/PageDisqus.body.php';
+	static function onSkinAfterContent( &$data, $skin )  {
+		global $wgPageDisqusShortname, $wgPageDisqusExclude, $wgTitle, $wgRequest;
 
-$wgHooks['SkinAfterContent'][] = 'PageDisqus::onSkinAfterContent';
-$wgHooks['SkinAfterBottomScripts'][] = 'PageDisqus::onSkinAfterBottomScripts';
+		$wgPageDisqusShortname = strtolower( $wgPageDisqusShortname );
+
+		if ( empty( $wgPageDisqusShortname ) ) {
+			$shortname = wfMessage( 'pagedisqus-shortname' )->text();
+			exit( $shortname );
+		}
+
+		if ( $wgRequest->getVal( 'action', 'view' ) != 'view' ) {
+			return true;
+		}
+
+		if ( ! empty( $wgPageDisqusExclude ) ) {
+			foreach ( $wgPageDisqusExclude as $excluded ) {
+				if ( preg_match( '/' . $excluded . '/', $wgTitle->getFullText() ) ) {
+					return true;
+				}
+			}
+		}
+
+		$title = wfMessage( 'pagedisqus-title' )->text();
+		$noscript = wfMessage( 'pagedisqus-noscript' )->text();
+
+		$page_url = $wgRequest->getFullRequestURL();
+		$page_identifier = $wgPageDisqusShortname;
+
+        $data = <<<HTML
+<br><hr>
+{$title}
+<div id="disqus_thread"></div>
+<script>
+    var disqus_config = function () {
+        this.page.url = "{$page_url}";
+        this.page.identifier = "{$page_identifier}";
+    };
+    (function() {
+        var d = document, s = d.createElement('script');
+
+        s.src = '//{$page_identifier}.disqus.com/embed.js';
+
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="//disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+HTML;
+	
+
+		return true;
+	}
+
+	static function onSkinAfterBottomScripts( $skin, &$text ) {
+		global $wgPageDisqusShortname, $wgPageDisqusExclude, $wgTitle, $wgRequest;
+
+		if ( $wgRequest->getVal( 'action', 'view' ) != 'view' ) {
+			return true;
+		}
+
+		if ( ! empty( $wgPageDisqusExclude ) ) {
+			foreach ( $wgPageDisqusExclude as $excluded ) {
+				if ( preg_match( '/' . $excluded . '/', $wgTitle->getFullText() ) ) {
+					return true;
+				}
+			}
+		}
+
+		if ( empty( $wgPageDisqusShortname ) ) {
+			$shortname = wfMessage( 'pagedisqus-shortname' )->text();
+			exit( $shortname );
+		}
+
+		$text .= "<script type=\"text/javascript\">
+			//<![CDATA[
+			(function()
+			{
+			var links = document.getElementsByTagName('a');
+			var query = '?';
+			for(var i = 0; i < links.length; i++)
+			if(links[i].href.indexOf('#disqus_thread') >= 0)
+			query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
+			document.write('<script charset=\"utf-8\" type=\"text/javascript\" src=\"//disqus.com/forums/" . $wgPageDisqusShortname . "/get_num_replies.js' + query + '\"></' + 'script>');
+			})();
+			//]]>
+			</script>";
+		return true;
+	}
+}
