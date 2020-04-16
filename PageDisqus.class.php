8d844a1606dec5d246e6798472efa8949554c92c
Sophivorus <schenonef@gmail.com>
2020-04-16 11:04:37 -0700

    Various things - Fix bug by which all pages showed the same comments. - Update methods and code style - Add $wgPageDisqusPageBlacklist to supersede $wgPageDisqusExcluded - Add $wgPageDisqusNamespaceWhitelist with NS_MAIN as only default
    Change-Id: I76479e223574e437cf3fc7d7beebfa1f8737f8f5

--- PageDisqus.class.php
+++ PageDisqus.class.php
@@ -3,46 +3,42 @@
 class PageDisqus {
 
 	static function onSkinAfterContent( &$data, $skin )  {
-		global $wgPageDisqusShortname, $wgPageDisqusExclude, $wgTitle, $wgRequest;
-
-		$wgPageDisqusShortname = strtolower( $wgPageDisqusShortname );
+		global $wgPageDisqusShortname, $wgPageDisqusNamespaceWhitelist, $wgPageDisqusPageBlacklist, $mediaWiki;
 
 		if ( empty( $wgPageDisqusShortname ) ) {
-			$shortname = wfMessage( 'pagedisqus-shortname' )->text();
-			exit( $shortname );
+			exit( wfMessage( 'pagedisqus-shortname' ) );
 		}
 
-		if ( $wgRequest->getVal( 'action', 'view' ) != 'view' ) {
+		if ( $mediaWiki->getAction() !== 'view' ) {
 			return true;
 		}
 
-		if ( ! empty( $wgPageDisqusExclude ) ) {
-			foreach ( $wgPageDisqusExclude as $excluded ) {
-				if ( preg_match( '/' . $excluded . '/', $wgTitle->getFullText() ) ) {
-					return true;
-				}
-			}
+		if ( !in_array( $skin->getTitle()->getNamespace(), $wgPageDisqusNamespaceWhitelist ) ) {
+			return true;
 		}
 
-		$title = wfMessage( 'pagedisqus-title' )->text();
-		$noscript = wfMessage( 'pagedisqus-noscript' )->text();
+		if ( in_array( $skin->getTitle()->getFullText(), $wgPageDisqusPageBlacklist ) ) {
+			return true;
+		}
 
-		$page_url = $wgRequest->getFullRequestURL();
-		$page_identifier = $wgPageDisqusShortname;
+		$title = wfMessage( 'pagedisqus-title' );
+		$noscript = wfMessage( 'pagedisqus-noscript' );
+
+		$pageURL = $skin->getTitle()->getFullURL();
+		$pageID = $skin->getTitle()->getArticleID();
 
         $data = <<<HTML
-<br><hr>
-{$title}
+<h2>{$title}</h2>
 <div id="disqus_thread"></div>
 <script>
     var disqus_config = function () {
-        this.page.url = "{$page_url}";
-        this.page.identifier = "{$page_identifier}";
+        this.page.url = "{$pageURL}";
+        this.page.identifier = "{$pageID}";
     };
     (function() {
         var d = document, s = d.createElement('script');
 
-        s.src = '//{$page_identifier}.disqus.com/embed.js';
+        s.src = '//{$wgPageDisqusShortname}.disqus.com/embed.js';
 
         s.setAttribute('data-timestamp', +new Date());
         (d.head || d.body).appendChild(s);
@@ -50,44 +46,44 @@ class PageDisqus {
 </script>
 <noscript>Please enable JavaScript to view the <a href="//disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
 HTML;
-
-
 		return true;
 	}
 
 	static function onSkinAfterBottomScripts( $skin, &$text ) {
-		global $wgPageDisqusShortname, $wgPageDisqusExclude, $wgTitle, $wgRequest;
+		global $wgPageDisqusShortname, $wgPageDisqusNamespaceWhitelist, $wgPageDisqusPageBlacklist, $mediaWiki;
 
 		if ( empty( $wgPageDisqusShortname ) ) {
-			wfWarn( wfMessage( 'pagedisqus-shortname' )->text() );
+			wfWarn( wfMessage( 'pagedisqus-shortname' ) );
 			return true;
 		}
 
-		if ( $wgRequest->getVal( 'action', 'view' ) != 'view' ) {
+		if ( $mediaWiki->getAction() !== 'view' ) {
 			return true;
 		}
 
-		if ( ! empty( $wgPageDisqusExclude ) ) {
-			foreach ( $wgPageDisqusExclude as $excluded ) {
-				if ( preg_match( '/' . $excluded . '/', $wgTitle->getFullText() ) ) {
-					return true;
-				}
-			}
+		if ( !in_array( $skin->getTitle()->getNamespace(), $wgPageDisqusNamespaceWhitelist ) ) {
+			return true;
 		}
 
-		$text .= "<script type=\"text/javascript\">
-			//<![CDATA[
-			(function()
-			{
-			var links = document.getElementsByTagName('a');
-			var query = '?';
-			for(var i = 0; i < links.length; i++)
-			if(links[i].href.indexOf('#disqus_thread') >= 0)
-			query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
-			document.write('<script charset=\"utf-8\" type=\"text/javascript\" src=\"//disqus.com/forums/" . $wgPageDisqusShortname . "/get_num_replies.js' + query + '\"></' + 'script>');
-			})();
-			//]]>
-			</script>";
+		if ( in_array( $skin->getTitle()->getFullText(), $wgPageDisqusPageBlacklist ) ) {
+			return true;
+		}
+
+		$text .= <<<SCRIPT
+<script>
+	//<![CDATA[
+	(function()
+	{
+	var links = document.getElementsByTagName('a');
+	var query = '?';
+	for(var i = 0; i < links.length; i++)
+	if(links[i].href.indexOf('#disqus_thread') >= 0)
+	query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
+	document.write('<script charset="utf-8" type="text/javascript" src="//disqus.com/forums/{$wgPageDisqusShortname}/get_num_replies.js' + query + '"></' + 'script>');
+	})();
+	//]]>
+</script>
+SCRIPT;
 		return true;
 	}
 }
